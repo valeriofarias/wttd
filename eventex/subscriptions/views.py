4e182bc1e7fe80ec94226bbb547d2a56351ad938
Valério Farias <valeriofc@gmail.com>
2021-03-24 19:14:37 -0300

    Configuração do campo from do email de acordo com o gmail

--- eventex/subscriptions/views.py
+++ eventex/subscriptions/views.py
@@ -15,8 +15,8 @@ def subscribe(request):
                                  
             mail.send_mail('Confirmação de inscrição', 
                        body,
-                       'contato@eventex.com.br',
-                       ['contato@eventex.com.br',
+                       'valeriofc@gmail.com',
+                       ['valeriofc@gmail.com',
                         form.cleaned_data['email']])
             
             messages.success(request, 'Inscrição realizada com sucesso!')
