17c226a91f7578e6e428c3cc62e416d9e181a85c
Valério Farias <valeriofc@gmail.com>
2021-04-03 01:24:51 -0300

    Thank you page

--- eventex/subscriptions/tests/test_view_subscribe.py
+++ eventex/subscriptions/tests/test_view_subscribe.py
@@ -46,8 +46,8 @@ class SubscribePostValid(TestCase):
         self.resp = self.client.post('/inscricao/', data)
         
     def test_post(self):
-        """Valid POST should redirect to /inscricao/"""
-        self.assertEqual(302, self.resp.status_code) 
+        """Valid POST should redirect to /inscricao/1/"""
+        self.assertRedirects(self.resp, '/inscricao/1/') 
         
     def test_send_subscribe_email(self):
         self.assertEqual(1, len(mail.outbox))
@@ -77,13 +77,6 @@ class SubscribePostInvalid(TestCase):
     
     def test_dont_save_subscription(self):
         self.assertFalse(Subscription.objects.exists())
-        
+       
 
-class SubscribeSuccessMessage(TestCase):
-    def test_message(self):
-        data = dict(name='Valério Farias', cpf='12345678901',
-                    email='valeriofc@gmail.com', phone='8488888888')
-        
-        response = self.client.post('/inscricao/', data, follow=True)
-        self.assertContains(response, 'Inscrição realizada com sucesso!') 
         
