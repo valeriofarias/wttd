c64f600f6bc183892a49b4b3b844011370ff93e4
Valério Farias <valeriofc@gmail.com>
2021-04-15 00:02:30 -0300

    Email ou telefone opcionais

--- eventex/subscriptions/forms.py
+++ eventex/subscriptions/forms.py
@@ -11,13 +11,19 @@ def validate_cpf(value):
 class SubscriptionForm(forms.Form):
     name = forms.CharField(label='Nome')
     cpf = forms.CharField(label='CPF', validators=[validate_cpf])
-    email = forms.EmailField(label='Email')
-    phone = forms.CharField(label='Telefone')
+    email = forms.EmailField(label='Email', required=False)
+    phone = forms.CharField(label='Telefone', required=False)
     
     
     def clean_name(self):
         name = self.cleaned_data['name'].lower()
         prep = {'da', 'das', 'de', 'do', 'dos'}
         words = (w.capitalize() if w not in prep else w for w in name.split())
-        return ' '.join(words)              
+        return ' '.join(words)
+        
+    def clean(self):
+        if not self.cleaned_data.get('email') and not self.cleaned_data.get('phone'):
+            raise ValidationError('Informe seu e-mail ou telefone.')  
+            
+        return self.cleaned_data           
 
